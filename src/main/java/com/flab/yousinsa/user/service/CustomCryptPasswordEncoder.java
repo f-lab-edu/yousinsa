56aa58157dc6e042d9c2631a2e7ec311c722cb6a
key <rlfbd5142@gmail.com>
2022-05-27 00:57:55 +0900

    [feature] Password 해싱을 담당하는 Encoder 정의
    - Spring Security를 바로 사용하는 것이 아니라 Custom한 Encoder Interface 정의
    -> 추후 Security Bean으로 교체 예정

---
+++ src/main/java/com/flab/yousinsa/user/service/CustomCryptPasswordEncoder.java
@@ -0,0 +1,38 @@
+package com.flab.yousinsa.user.service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class CustomCryptPasswordEncoder implements PasswordEncoder{
+
+	private final String SHA_512 = "SHA-512";
+
+	@Override
+	public String hashPassword(String rawPassword) {
+		return hashPasswordWithMessageDigest(rawPassword, SHA_512);
+	}
+
+	@Override
+	public Boolean isMatched(String rawPassword, String hashedPassword) {
+		String hashedRawpassword = hashPasswordWithMessageDigest(rawPassword, SHA_512);
+
+		return hashedRawpassword.equals(hashedPassword);
+	}
+
+	private String hashPasswordWithMessageDigest(String rawPassword, String algorithm) {
+		String hashedPassword;
+		try {
+			MessageDigest digest = MessageDigest.getInstance(algorithm);
+			digest.reset();
+			digest.update(rawPassword.getBytes(StandardCharsets.UTF_8));
+			hashedPassword = Arrays.toString(digest.digest());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+
+		return hashedPassword;
+	}
+}
