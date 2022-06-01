ea243c4ccb1e285f78c1dbb2f12b394c1100b140
key <rlfbd5142@gmail.com>
2022-06-01 23:06:13 +0900

    [fix] ControllerAdvice 방식에서 ResponseStatus를 Exception에 추가하는 것으로 변경

--- src/main/java/com/flab/yousinsa/user/controller/RestControllerAdvice.java
+++ src/main/java/com/flab/yousinsa/user/controller/RestControllerAdvice.java
@@ -1,21 +1,8 @@
 package com.flab.yousinsa.user.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.flab.yousinsa.user.service.exception.SignUpFailException;
 
 @ControllerAdvice
 public class RestControllerAdvice {
 
-	@ExceptionHandler(SignUpFailException.class)
-	public ResponseEntity<String> signUpFailed(Exception e) {
-
-		// TODO ErrorResponse -> ResponseBody 추상화
-		return ResponseEntity
-			.status(HttpStatus.CONFLICT)
-			.body("signUp Process Failed due to :: " + e.getMessage());
-	}
 }
--- src/main/java/com/flab/yousinsa/user/service/exception/SignUpFailException.java
+++ src/main/java/com/flab/yousinsa/user/service/exception/SignUpFailException.java
@@ -1,5 +1,9 @@
 package com.flab.yousinsa.user.service.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class SignUpFailException extends RuntimeException {
 	public SignUpFailException(String message) {
 		super(message);
