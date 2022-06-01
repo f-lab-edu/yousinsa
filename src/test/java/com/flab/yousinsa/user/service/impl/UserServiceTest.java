590710b8c8fd546a379cb6862dd87360920e92b9
key <rlfbd5142@gmail.com>
2022-06-01 22:00:41 +0900

    [test-refactor] UserService Test Code Refactoring
    - System.out.println 삭제 -> 성능적으로 안 좋은 영향
    - 공통적으로 사용하는 UserEntity 추출
    - BDD 형식에 맞춰서 BDDMockito 방식으로 변경

--- src/test/java/com/flab/yousinsa/user/service/impl/UserServiceTest.java
+++ src/test/java/com/flab/yousinsa/user/service/impl/UserServiceTest.java
@@ -1,14 +1,17 @@
 package com.flab.yousinsa.user.service.impl;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -33,30 +36,34 @@ class UserServiceTest {
 	@Mock
 	PasswordEncoder passwordEncoder;
 
+	@InjectMocks
+	UserService userService;
+
+	UserEntity user;
+
+	@BeforeEach
+	public void setUp() {
+		user = new UserEntity("key", "rlfbd5142@gmail.com", "hashedPassword", UserRole.BUYER);
+	}
+
 	@Test
 	@DisplayName("같은 이메일을 가진 유저가 없어 정상적으로 회원가입 되는 경우")
 	public void signUpNoUserEmailTest() {
 		// given
-		UserEntity user = new UserEntity("key", "rlfbd5142@gmail.com", "hashedPassword", UserRole.BUYER);
 		SignUpRequestDto signUpRequestDto = new SignUpRequestDto(user.getUserName(), user.getUserEmail(),
 			"password", user.getUserRole());
 		SignUpResponseDto signUpResponseDto = new SignUpResponseDto(1L, user.getUserName(), user.getUserEmail(),
 			user.getUserRole());
 
-		when(userRepository.save(any(UserEntity.class))).thenReturn(user);
-		when(signUpDtoConverter.convertSignUpRequestToUser(any(SignUpRequestDto.class))).thenReturn(user);
-		when(signUpDtoConverter.convertUserToSignUpResponse(any(UserEntity.class))).thenReturn(signUpResponseDto);
-		when(passwordEncoder.hashPassword(signUpRequestDto.getUserPassword())).thenReturn("hashedPassword");
-
-		UserService userService = new UserService(userRepository, signUpDtoConverter, passwordEncoder);
+		given(userRepository.save(any(UserEntity.class))).willReturn(user);
+		given(signUpDtoConverter.convertSignUpRequestToUser(any(SignUpRequestDto.class))).willReturn(user);
+		given(signUpDtoConverter.convertUserToSignUpResponse(any(UserEntity.class))).willReturn(signUpResponseDto);
+		given(passwordEncoder.hashPassword(signUpRequestDto.getUserPassword())).willReturn("hashedPassword");
 
 		// when
 		SignUpResponseDto resultResponse = userService.trySignUpUser(signUpRequestDto);
 
 		// then
-		System.out.println("resultResponse.getUserName() = " + resultResponse.getUserName());
-		System.out.println("resultResponse.getUserEmail() = " + resultResponse.getUserEmail());
-		System.out.println("resultResponse.getUserRole() = " + resultResponse.getUserRole());
 		assertThat(resultResponse.getUserName()).isEqualTo(signUpResponseDto.getUserName());
 		assertThat(resultResponse.getUserEmail()).isEqualTo(signUpResponseDto.getUserEmail());
 		assertThat(resultResponse.getUserRole()).isEqualTo(signUpResponseDto.getUserRole());
@@ -66,17 +73,9 @@ class UserServiceTest {
 	@DisplayName("같은 이메일을 가진 유저가 있어 회원가입이 안되는 경우")
 	public void signUpSameEmailUserExistTest() {
 		// given
-		UserEntity user = new UserEntity("key", "rlfbd5142@gmail.com", "hashedPassword", UserRole.BUYER);
-
 		SignUpRequestDto signUpRequestDto = new SignUpRequestDto(user.getUserName(), user.getUserEmail(),
 			"password", user.getUserRole());
-		SignUpResponseDto signUpResponseDto = new SignUpResponseDto(1L, user.getUserName(), user.getUserEmail(),
-			user.getUserRole());
-
-		// 필요가 없는 Mock 객체는 필요없다고 Test 실패
-		when(userRepository.findByUserEmail(any(String.class))).thenReturn(Optional.of(user));
-
-		UserService userService = new UserService(userRepository, signUpDtoConverter, passwordEncoder);
+		given(userRepository.findByUserEmail(any(String.class))).willReturn(Optional.of(user));
 
 		// when, then
 		Assertions.assertThrows(SignUpFailException.class, () -> {
