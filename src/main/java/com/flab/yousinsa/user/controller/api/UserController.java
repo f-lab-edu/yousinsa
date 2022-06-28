e3dbf2a2b7c35bb19e691609063d777e59a47621
key <rlfbd5142@gmail.com>
2022-06-28 19:11:38 +0900

    [feature] 회원 탈퇴 Handler 구현
    - 회원탈퇴에 대한 테스트와 RestDoc 생성
    - RestDoc 생성과 관련된 테스트 명 수정
    - Interceptor 테스트에 대한 URL 수정
    - 회원탈퇴 API/사용된 HttpStatus index.adoc 추가

--- src/main/java/com/flab/yousinsa/user/controller/api/UserController.java
+++ src/main/java/com/flab/yousinsa/user/controller/api/UserController.java
@@ -3,10 +3,15 @@ package com.flab.yousinsa.user.controller.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.flab.yousinsa.user.controller.annotation.SessionAuth;
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.dtos.request.SignInRequestDto;
 import com.flab.yousinsa.user.domain.dtos.request.SignUpRequestDto;
 import com.flab.yousinsa.user.domain.dtos.response.SignUpResponseDto;
 import com.flab.yousinsa.user.service.contract.UserSignUpService;
@@ -25,5 +30,46 @@ public class UserController {
 	public ResponseEntity<SignUpResponseDto> signUpUser(@RequestBody SignUpRequestDto signUpRequest) {
 		SignUpResponseDto signUpResponse = userService.trySignUpUser(signUpRequest);
 		return ResponseEntity.ok(signUpResponse);
+	}
+
+	@PostMapping("api/v1/users/sign-in")
+	public ResponseEntity<SignInResponseDto> signInUser(@Valid @RequestBody SignInRequestDto signInRequestDto,
+		HttpSession httpSession) {
+		SignInResponseDto signInResponseDto = userSignInService.trySignInUser(signInRequestDto);
+		AuthUser authUser = new AuthUser(
+			signInResponseDto.getId(),
+			signInResponseDto.getUserName(),
+			signInResponseDto.getUserEmail(),
+			signInResponseDto.getUserRole()
+		);
+		httpSession.setAttribute(AUTH_USER, authUser);
+
+		return ResponseEntity.ok(signInResponseDto);
+	}
+
+	@DeleteMapping("api/v1/users/sign-out")
+	public ResponseEntity<Void> signOutUser(HttpSession httpSession) {
+		try {
+			httpSession.invalidate();
+		} catch (IllegalStateException e) {
+			if (log.isErrorEnabled()) {
+				log.error("User already signed out", e);
+			}
+
+			throw new SignOutFailException("User already signed out", e);
+		}
+
+		return ResponseEntity.status(HttpStatus.RESET_CONTENT).build();
+	}
+
+	@SessionAuth
+	@DeleteMapping("api/v1/users/{userId}")
+	public ResponseEntity<Void> withDrawUser(AuthUser user, @PathVariable("userId") Long userId,
+		HttpSession httpSession) {
+		userSignUpService.tryWithdrawUser(user, userId);
+
+		httpSession.invalidate();
+
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
--- src/test/java/com/flab/yousinsa/user/controller/api/UserControllerTest.java
+++ src/test/java/com/flab/yousinsa/user/controller/api/UserControllerTest.java
@@ -4,6 +4,8 @@ import static com.flab.yousinsa.ApiDocumentUtils.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -18,6 +20,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -53,7 +56,7 @@ class UserControllerTest {
 	}
 
 	@Test
-	@DisplayName("회원가입 API")
+	@DisplayName("회원가입 API Doc")
 	public void signUp() throws Exception {
 		// given
 		SignUpRequestDto signUpRequestDto = new SignUpRequestDto(user.getUserName(), user.getUserEmail(),
@@ -94,5 +97,145 @@ class UserControllerTest {
 			);
 
 		then(userSignUpService).should().trySignUpUser(refEq(signUpRequestDto));
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("로그인 API Doc")
+	public void signIn() throws Exception {
+		// given
+		SignInRequestDto signInRequestDto = new SignInRequestDto(user.getUserEmail(), rawPassword);
+		SignInResponseDto signInResponseDto = new SignInResponseDto(1L, user.getUserName(), user.getUserEmail(),
+			user.getUserRole());
+
+		MockHttpSession mockHttpSession = new MockHttpSession();
+
+		given(userSignInService.trySignInUser(any(SignInRequestDto.class)))
+			.willReturn(signInResponseDto);
+
+		// when
+		ResultActions resultActions = mockMvc.perform(post("/api/v1/users/sign-in").session(mockHttpSession)
+			.content(objectMapper.writeValueAsString(signInRequestDto))
+			.contentType(MediaType.APPLICATION_JSON)
+			.accept(MediaType.APPLICATION_JSON)
+		);
+
+		// then
+		resultActions.andExpect(status().isOk())
+			.andExpect(content().string(objectMapper.writeValueAsString(signInResponseDto)))
+			.andDo(
+				document("user-signin",
+					getDocumentRequest(),
+					getDocumentResponse(),
+					requestFields(
+						fieldWithPath("userEmail").type(JsonFieldType.STRING).description("로그인 하고자 하는 유저 이메일"),
+						fieldWithPath("userPassword").type(JsonFieldType.STRING).description("로그인 하고자 하는 유저 비밀번호")
+					),
+					responseFields(
+						fieldWithPath("id").type(JsonFieldType.NUMBER).description("로그인된 유저 아이디(pk)"),
+						fieldWithPath("userName").type(JsonFieldType.STRING).description("로그인된 유저 이름"),
+						fieldWithPath("userEmail").type(JsonFieldType.STRING).description("로그인된 유저 이메일"),
+						fieldWithPath("userRole").type(JsonFieldType.STRING).description("로그인된 유저 역할")
+					)
+				)
+			);
+
+		AuthUser authUser = (AuthUser)mockHttpSession.getAttribute(AuthWebConfig.Session.AUTH_USER);
+		assertThat(authUser.getUserEmail()).isEqualTo(signInResponseDto.getUserEmail());
+		assertThat(authUser.getUserName()).isEqualTo(signInResponseDto.getUserName());
+		assertThat(authUser.getUserRole()).isEqualTo(signInResponseDto.getUserRole());
+
+		then(userSignInService).should().trySignInUser(refEq(signInRequestDto));
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("로그아웃 API Doc")
+	public void signOut() throws Exception {
+		// given
+		MockHttpSession mockHttpSession = new MockHttpSession();
+		SignInResponseDto signInResponseDto = new SignInResponseDto(1L, user.getUserName(), user.getUserEmail(),
+			user.getUserRole());
+		mockHttpSession.setAttribute(AuthWebConfig.Session.AUTH_USER, signInResponseDto);
+
+		// when
+		ResultActions resultActions = mockMvc.perform(delete("/api/v1/users/sign-out").session(mockHttpSession));
+
+		// then
+		resultActions.andExpect(status().isResetContent())
+			.andDo(
+				document("user-signout",
+					getDocumentRequest(),
+					getDocumentResponse())
+			);
+
+		assertThatThrownBy(() -> mockHttpSession.getAttribute(AuthWebConfig.Session.AUTH_USER))
+			.isInstanceOf(IllegalStateException.class);
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("회원탈퇴 API Doc")
+	public void withDraw() throws Exception {
+		// given
+		MockHttpSession mockHttpSession = new MockHttpSession();
+		Long deleteTargetUserId = 1L;
+		AuthUser authUser = new AuthUser(deleteTargetUserId, user.getUserName(), user.getUserEmail(),
+			user.getUserRole());
+		mockHttpSession.setAttribute(AuthWebConfig.Session.AUTH_USER, authUser);
+		willDoNothing().given(userSignUpService).tryWithdrawUser(any(AuthUser.class), anyLong());
+
+		// when
+		ResultActions resultActions = mockMvc.perform(
+			RestDocumentationRequestBuilders.delete("/api/v1/users/{userId}", deleteTargetUserId).session(
+				mockHttpSession));
+
+		// then
+		resultActions.andExpect(status().isNoContent())
+			.andDo(
+				document("user-withdraw",
+					getDocumentRequest(),
+					getDocumentResponse(),
+					pathParameters(
+						parameterWithName("userId").description("userId of withdraw requested user")
+							.attributes(key("type").value(LONG))
+					)
+				)
+			);
+
+		then(userSignUpService).should().tryWithdrawUser(refEq(authUser), refEq(deleteTargetUserId));
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("이미 로그아웃시 로그아웃 실패")
+	public void signOutFail() throws Exception {
+		// given
+		// NoSession
+
+		// when
+		ResultActions resultActions = mockMvc.perform(delete("/api/v1/users/sign-out"));
+
+		// then
+		resultActions.andExpect(status().isUnauthorized());
+	}
+
+	/**
+	 * 해당 되는 URL 패턴의 API가 있을 경우 Method가 다르면 401이 아니라 Method_Not_Allowed(405)가 반환됨
+	 * 따라서 이 테스트가 변동 사항이 없도록 만들 예정이 없는 URL로 테스트를 진행
+	 * @throws Exception
+	 */
+	@UnitTest
+	@Test
+	@DisplayName("회원가입, 로그인이 아닌 API에 대해서 Session이 없는 경우 인증되지 않음(401) 반환")
+	public void authRejectByAuthUserInterceptor() throws Exception {
+		// given
+		// NoSession
+
+		// when
+		ResultActions resultActions = mockMvc.perform(get("/api/test"));
+
+		// then
+		resultActions.andExpect(status().isUnauthorized());
 	}
 }
