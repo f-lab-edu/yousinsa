d57a11941823e4a0b9e8ac4ac0d55c865c1af96b
key <rlfbd5142@gmail.com>
2022-06-28 19:10:42 +0900

    [feature] 회원탈퇴 Service 로직 구현
    - 로그인 된 상태에서 자신에 대해서 회원탈퇴를 신청할 수 있음

---
+++ src/main/java/com/flab/yousinsa/user/service/impl/UserSignUpServiceImpl.java
@@ -0,0 +1,77 @@
+package com.flab.yousinsa.user.service.impl;
+
+import java.util.Objects;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.dtos.request.SignUpRequestDto;
+import com.flab.yousinsa.user.domain.dtos.response.SignUpResponseDto;
+import com.flab.yousinsa.user.domain.entities.UserEntity;
+import com.flab.yousinsa.user.repository.contract.UserRepository;
+import com.flab.yousinsa.user.service.PasswordEncoder;
+import com.flab.yousinsa.user.service.contract.UserSignUpService;
+import com.flab.yousinsa.user.service.converter.SignUpDtoConverter;
+import com.flab.yousinsa.user.service.exception.AuthException;
+import com.flab.yousinsa.user.service.exception.SignUpFailException;
+import com.flab.yousinsa.user.service.exception.WithdrawFailException;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Transactional
+@Slf4j
+public class UserSignUpServiceImpl implements UserSignUpService {
+
+	private final UserRepository userRepository;
+	private final SignUpDtoConverter signUpDtoConverter;
+	private final PasswordEncoder passwordEncoder;
+
+	public UserSignUpServiceImpl(UserRepository userRepository, SignUpDtoConverter signUpDtoConverter,
+		PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.signUpDtoConverter = signUpDtoConverter;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	public SignUpResponseDto trySignUpUser(SignUpRequestDto signUpRequest) {
+		Assert.notNull(signUpRequest, "SignUpRequest must be not null");
+		validateSignUpUser(signUpRequest);
+
+		String hashedPassword = passwordEncoder.hashPassword(signUpRequest.getUserPassword());
+
+		UserEntity newUser = signUpDtoConverter.convertSignUpRequestToUser(signUpRequest, hashedPassword);
+
+		UserEntity savedUser = userRepository.save(newUser);
+
+		return signUpDtoConverter.convertUserToSignUpResponse(savedUser);
+	}
+
+	@Override
+	public void tryWithdrawUser(AuthUser user, Long withdrawUserId) {
+		Assert.notNull(user, "To withdraw user, user must be logined");
+		Assert.notNull(withdrawUserId, "To withdraw user, valid withdrawUserId must given");
+
+		if (!Objects.equals(user.getId(), withdrawUserId)) {
+			throw new AuthException("Withdrawn userId must be equal to logined UserId");
+		}
+
+		UserEntity userEntity = userRepository.findById(withdrawUserId).orElseThrow(
+			() -> new WithdrawFailException("Withdraw user id does not exist")
+		);
+
+		userRepository.delete(userEntity);
+	}
+
+	private void validateSignUpUser(SignUpRequestDto signUpRequest) {
+		boolean isPresent = userRepository.findByUserEmail(signUpRequest.getUserEmail()).isPresent();
+		if (isPresent) {
+			log.info("validateSignUser :: " + signUpRequest.getUserEmail());
+			throw new SignUpFailException("request email is already exists");
+		}
+	}
+
+}
---
+++ src/test/java/com/flab/yousinsa/user/service/impl/UserSignUpServiceImplTest.java
@@ -0,0 +1,161 @@
+package com.flab.yousinsa.user.service.impl;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.flab.yousinsa.annotation.UnitTest;
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.dtos.request.SignUpRequestDto;
+import com.flab.yousinsa.user.domain.dtos.response.SignUpResponseDto;
+import com.flab.yousinsa.user.domain.entities.UserEntity;
+import com.flab.yousinsa.user.domain.enums.UserRole;
+import com.flab.yousinsa.user.repository.contract.UserRepository;
+import com.flab.yousinsa.user.service.PasswordEncoder;
+import com.flab.yousinsa.user.service.converter.SignUpDtoConverter;
+import com.flab.yousinsa.user.service.exception.AuthException;
+import com.flab.yousinsa.user.service.exception.SignUpFailException;
+
+@ExtendWith(MockitoExtension.class)
+class UserSignUpServiceImplTest {
+
+	@Mock
+	UserRepository userRepository;
+
+	@Mock
+	SignUpDtoConverter signUpDtoConverter;
+
+	@Mock
+	PasswordEncoder passwordEncoder;
+
+	@InjectMocks
+	UserSignUpServiceImpl userSignUpServiceImpl;
+
+	UserEntity user;
+
+	final String HashedPassword = "hashedPassword";
+
+	@BeforeEach
+	public void setUp() {
+		user = new UserEntity("key", "rlfbd5142@gmail.com", "hashedPassword", UserRole.BUYER);
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("같은 이메일을 가진 유저가 없어 정상적으로 회원가입 되는 경우")
+	public void signUpNoUserEmailTest() {
+		// given
+		SignUpRequestDto signUpRequestDto = new SignUpRequestDto(user.getUserName(), user.getUserEmail(),
+			"password", user.getUserRole());
+		SignUpResponseDto signUpResponseDto = new SignUpResponseDto(1L, user.getUserName(), user.getUserEmail(),
+			user.getUserRole());
+
+		given(userRepository.save(any(UserEntity.class))).willReturn(user);
+		given(passwordEncoder.hashPassword(signUpRequestDto.getUserPassword())).willReturn(HashedPassword);
+		given(signUpDtoConverter.convertSignUpRequestToUser(any(SignUpRequestDto.class), eq(HashedPassword)))
+			.willReturn(user);
+		given(signUpDtoConverter.convertUserToSignUpResponse(any(UserEntity.class))).willReturn(signUpResponseDto);
+
+		// when
+		SignUpResponseDto resultResponse = userSignUpServiceImpl.trySignUpUser(signUpRequestDto);
+
+		// then
+		then(userRepository).should().save(user);
+		then(passwordEncoder).should().hashPassword(signUpRequestDto.getUserPassword());
+		then(signUpDtoConverter).should().convertSignUpRequestToUser(signUpRequestDto, HashedPassword);
+		then(signUpDtoConverter).should().convertUserToSignUpResponse(user);
+
+		assertThat(resultResponse.getUserName()).isEqualTo(signUpResponseDto.getUserName());
+		assertThat(resultResponse.getUserEmail()).isEqualTo(signUpResponseDto.getUserEmail());
+		assertThat(resultResponse.getUserRole()).isEqualTo(signUpResponseDto.getUserRole());
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("같은 이메일을 가진 유저가 있어 회원가입이 안되는 경우")
+	public void signUpSameEmailUserExistTest() {
+		// given
+		SignUpRequestDto signUpRequestDto = new SignUpRequestDto(user.getUserName(), user.getUserEmail(),
+			"password", user.getUserRole());
+		given(userRepository.findByUserEmail(any(String.class))).willReturn(Optional.of(user));
+
+		// when, then
+		Assertions.assertThatThrownBy(() -> {
+				SignUpResponseDto savedUser = userSignUpServiceImpl.trySignUpUser(signUpRequestDto);
+			}).isInstanceOf(SignUpFailException.class)
+			.hasMessageContaining("request email is already exists");
+
+		then(userRepository).should().findByUserEmail(signUpRequestDto.getUserEmail());
+		then(userRepository).should(never()).save(user);
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("로그인 하지 않은 상태로 회원 탈퇴 시도")
+	public void withDrawUserIfNoLogin() {
+		long withDrawUserId = 1L;
+		Assertions.assertThatThrownBy(
+				() -> userSignUpServiceImpl.tryWithdrawUser(null, withDrawUserId)
+			).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("To withdraw user, user must be logined");
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("로그인 한 상태로 유효하지 않은 userId 시도")
+	public void withDrawUserIfLoginInvalidWithdrawUserId() {
+		// given
+		AuthUser authUser = new AuthUser(2L, "mockName", "mockEmail@gmail.com", UserRole.BUYER);
+		Long withDrawUserId = null;
+
+		Assertions.assertThatThrownBy(
+				() -> userSignUpServiceImpl.tryWithdrawUser(authUser, withDrawUserId)
+			).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("To withdraw user, valid withdrawUserId must given");
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("로그인 한 상태로 해당 유저가 아닌 회원 탈퇴 시도")
+	public void withDrawUserIfLoginButDifferentUserId() {
+		// given
+		AuthUser authUser = new AuthUser(2L, "mockName", "mockEmail@gmail.com", UserRole.BUYER);
+
+		// when
+		long withDrawUserId = 1L;
+		Assertions.assertThatThrownBy(
+				() -> userSignUpServiceImpl.tryWithdrawUser(authUser, withDrawUserId)
+			).isInstanceOf(AuthException.class)
+			.hasMessageContaining("Withdrawn userId must be equal to logined UserId");
+	}
+
+	@UnitTest
+	@Test
+	@DisplayName("로그인 한 상태로 해당 유저 회원 탈퇴 시도")
+	public void withDrawUserIfLoginButSameUserId() {
+		// given
+		long withDrawUserId = 1L;
+
+		Optional<UserEntity> userEntityOptional = Optional.of(user);
+		given(userRepository.findById(anyLong())).willReturn(userEntityOptional);
+		willDoNothing().given(userRepository).delete(any(UserEntity.class));
+		AuthUser authUser = new AuthUser(withDrawUserId, user.getUserName(), user.getUserEmail(), UserRole.BUYER);
+
+		// when
+		userSignUpServiceImpl.tryWithdrawUser(authUser, withDrawUserId);
+
+		then(userRepository).should().findById(withDrawUserId);
+		then(userRepository).should().delete(eq(user));
+	}
+
+}
