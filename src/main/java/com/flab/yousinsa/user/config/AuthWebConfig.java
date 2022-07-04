33ac481828100a5bedc1d9d4e7740b1358f0a61a
key <rlfbd5142@gmail.com>
2022-07-05 00:00:37 +0900

    [feature] 로그인 한 User에 대한 정보를 Session에서 갖고 오는 로직 추가
    - HandlerMethodArgumentResolver 추가

--- src/main/java/com/flab/yousinsa/user/config/AuthWebConfig.java
+++ src/main/java/com/flab/yousinsa/user/config/AuthWebConfig.java
@@ -1,9 +1,13 @@
 package com.flab.yousinsa.user.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.flab.yousinsa.user.controller.component.AuthUserHandlerMethodArgumentResolver;
 import com.flab.yousinsa.user.controller.interceptor.AuthUserInterceptor;
 
 @Configuration
@@ -14,6 +18,11 @@ public class AuthWebConfig implements WebMvcConfigurer {
 		registry.addInterceptor(new AuthUserInterceptor())
 			.addPathPatterns("/**")
 			.excludePathPatterns("/**/users/sign-up", "/**/users/sign-in");
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(new AuthUserHandlerMethodArgumentResolver());
 	}
 
 	public static class Session {
