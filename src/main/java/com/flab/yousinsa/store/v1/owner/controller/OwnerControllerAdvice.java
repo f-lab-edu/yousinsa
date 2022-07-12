c4b7daa2b3b4c7db72028c36cdb0dd376eafbc36
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-13 00:00:17 +0900

    :construction: 비즈니스 로직 진행중

--- src/main/java/com/flab/yousinsa/store/v1/owner/controller/OwnerControllerAdvice.java
+++ src/main/java/com/flab/yousinsa/store/v1/owner/controller/OwnerControllerAdvice.java
@@ -1,4 +1,10 @@
 package com.flab.yousinsa.store.v1.owner.controller;
 
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
 public class OwnerControllerAdvice {
 }
