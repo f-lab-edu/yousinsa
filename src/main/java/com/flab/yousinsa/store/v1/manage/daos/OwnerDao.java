cd29198baccfc171233ac553e94d456162b47ba8
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-13 18:11:04 +0900

    :sparkles: 패키지 구조 설계용 클래스

---
+++ src/main/java/com/flab/yousinsa/store/v1/manage/daos/OwnerDao.java
@@ -0,0 +1,4 @@
+package com.flab.yousinsa.store.v1.manage.daos;
+
+public interface OwnerDao {
+}
---
+++ src/main/java/com/flab/yousinsa/store/v1/manage/dtos/OwnerDto.java
@@ -0,0 +1,13 @@
+package com.flab.yousinsa.store.v1.manage.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class OwnerDto {
+
+	@Setter
+	@Getter
+	public static class Post {
+		private String storeName;
+	}
+}
