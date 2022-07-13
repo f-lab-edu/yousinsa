63d351334db861de861fdbf70da534686a1efa95
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-13 14:20:38 +0900

    :sparkles: NotValidOwnerException 예외 핸들링 추가

--- src/main/java/com/flab/yousinsa/store/v1/owner/controller/OwnerControllerAdvice.java
+++ src/main/java/com/flab/yousinsa/store/v1/owner/controller/OwnerControllerAdvice.java
@@ -1,10 +1,17 @@
 package com.flab.yousinsa.store.v1.owner.controller;
 
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import lombok.extern.slf4j.Slf4j;
+import com.flab.yousinsa.store.exceptions.owner.NotValidOwnerException;
 
-@Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class OwnerControllerAdvice {
+
+	@ExceptionHandler(NotValidOwnerException.class)
+	public ResponseEntity<?> notValidOwnerException(NotValidOwnerException exception) {
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+	}
 }
