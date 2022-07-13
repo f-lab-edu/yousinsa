c963165089a10850009fde0ca40211464ed9401d
Kevin Ahn <maketheworldwise@gmail.com>
2022-07-13 14:25:14 +0900

    :truck: 패키지명 수정 및 이동
    - buyer -> purchase
    - owner -> manage
    - common package -> global package

--- src/main/java/com/flab/yousinsa/store/v1/owner/daos/OwnerDaoImpl.java
+++
@@ -1,4 +0,0 @@
-package com.flab.yousinsa.store.v1.owner.daos;
-
-public class OwnerDaoImpl implements OwnerDao {
-}
--- src/main/java/com/flab/yousinsa/store/v1/owner/service/OwnerService.java
+++
@@ -1,9 +0,0 @@
-package com.flab.yousinsa.store.v1.owner.service;
-
-import com.flab.yousinsa.store.v1.owner.dtos.OwnerDto;
-import com.flab.yousinsa.user.domain.entities.UserEntity;
-
-public interface OwnerService {
-	// 입점 신청
-	Long entryStore(OwnerDto.Post request, UserEntity user);
-}
