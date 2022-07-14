7e4d28721a54b9440aacecb45f1f80c214e25ff6
key <rlfbd5142@gmail.com>
2022-07-14 18:01:07 +0900

    [feature] Admin 입점 수락 Handler 구현
    [fix] JpaAuditingConfig 중복 선언 해결
    
    
    [fix] Controller에서 User Role 체크하도록 수정

---
+++ src/main/java/com/flab/yousinsa/admin/controller/api/AdminRequestStoreController.java
@@ -0,0 +1,45 @@
+package com.flab.yousinsa.admin.controller.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.yousinsa.admin.domain.dtos.RequestStoreDtoRequest;
+import com.flab.yousinsa.admin.domain.dtos.RequestStoreDtoResponse;
+import com.flab.yousinsa.admin.service.contract.AdminStoreRequestService;
+import com.flab.yousinsa.admin.service.exceptions.NotPermittedException;
+import com.flab.yousinsa.user.controller.annotation.SessionAuth;
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.enums.UserRole;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class AdminRequestStoreController {
+
+	private final AdminStoreRequestService storeRequestService;
+
+	public AdminRequestStoreController(AdminStoreRequestService storeRequestService) {
+		this.storeRequestService = storeRequestService;
+	}
+
+	@SessionAuth
+	@PutMapping("api/admin/v1/store")
+	public ResponseEntity<RequestStoreDtoResponse> acceptStoreRequest(
+		@RequestBody RequestStoreDtoRequest requestStoreDtoRequest, AuthUser user
+	) {
+		isAuthUserAdmin(user);
+		
+		RequestStoreDtoResponse response = storeRequestService.acceptStoreRequest(requestStoreDtoRequest);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+	}
+
+	private void isAuthUserAdmin(AuthUser user) {
+		if (user.getUserRole() != UserRole.ADMIN) {
+			throw new NotPermittedException("store request can be accepted by admin");
+		}
+	}
+}
