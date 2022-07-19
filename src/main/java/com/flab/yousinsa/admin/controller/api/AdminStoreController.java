dad0659044cfe05c8310cc1bb0c312976b3356bd
key <rlfbd5142@gmail.com>
2022-07-19 14:37:51 +0900

    [feature] 모든 등록된 Store 조회 시 사용되는 Handler
    - ADMIN에 대한 검증을 Controller에서 수행
    [fix] REST API 형식에 맞게 url 수정

---
+++ src/main/java/com/flab/yousinsa/admin/controller/api/AdminStoreController.java
@@ -0,0 +1,40 @@
+package com.flab.yousinsa.admin.controller.api;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.yousinsa.admin.domain.dtos.AdminStoreDto;
+import com.flab.yousinsa.admin.service.contract.AdminStoreService;
+import com.flab.yousinsa.admin.service.exceptions.NotPermittedException;
+import com.flab.yousinsa.user.controller.annotation.SessionAuth;
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.enums.UserRole;
+
+@RestController
+public class AdminStoreController {
+
+	private final AdminStoreService adminStoreService;
+
+	public AdminStoreController(AdminStoreService adminStoreService) {
+		this.adminStoreService = adminStoreService;
+	}
+
+	@SessionAuth
+	@GetMapping("/api/admin/v1/stores")
+	public ResponseEntity<Page<AdminStoreDto>> listRegisteredStore(Pageable pageable, AuthUser user) {
+		isAuthUserAdmin(user);
+
+		Page<AdminStoreDto> allStores = adminStoreService.getAllStores(pageable);
+
+		return ResponseEntity.ok(allStores);
+	}
+
+	private void isAuthUserAdmin(AuthUser user) {
+		if (user.getUserRole() != UserRole.ADMIN) {
+			throw new NotPermittedException("store list can be viewed by admin");
+		}
+	}
+}
