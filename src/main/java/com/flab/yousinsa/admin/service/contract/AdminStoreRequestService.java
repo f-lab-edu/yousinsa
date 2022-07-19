737307d00ac499d920e40382ee303878f5c61b61
key <rlfbd5142@gmail.com>
2022-07-19 12:04:53 +0900

    [fix] REST API에 맞추기 위해 Controller 변경 부분 Service Layer 반영

--- src/main/java/com/flab/yousinsa/admin/service/contract/AdminStoreRequestService.java
+++ src/main/java/com/flab/yousinsa/admin/service/contract/AdminStoreRequestService.java
@@ -1,8 +1,7 @@
 package com.flab.yousinsa.admin.service.contract;
 
-import com.flab.yousinsa.admin.domain.dtos.RequestStoreDtoRequest;
 import com.flab.yousinsa.admin.domain.dtos.RequestStoreDtoResponse;
 
 public interface AdminStoreRequestService {
-	RequestStoreDtoResponse acceptStoreRequest(RequestStoreDtoRequest requestStoreDtoRequest);
+	RequestStoreDtoResponse acceptStoreRequest(Long storeId);
 }
