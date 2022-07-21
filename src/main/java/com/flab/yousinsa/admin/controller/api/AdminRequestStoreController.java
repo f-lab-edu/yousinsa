d0e5810fcedca2fff62b192743a8aca25ebcf729
key <rlfbd5142@gmail.com>
2022-07-21 11:42:17 +0900

    [feature] Admin 입점 수락 Handler 구현
    [fix] JpaAuditingConfig 중복 선언 해결
    
    
    [fix] Controller에서 User Role 체크하도록 수정
    
    
    [fix] REST API에 맞춰 수정
    
    
    [fix] AOP를 사용한 로그인 방식으로 변경

---
+++ src/main/java/com/flab/yousinsa/admin/controller/api/AdminRequestStoreController.java
@@ -0,0 +1,46 @@
+package com.flab.yousinsa.admin.controller.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.flab.yousinsa.admin.domain.dtos.RequestStoreDtoResponse;
+import com.flab.yousinsa.admin.service.contract.AdminStoreRequestService;
+import com.flab.yousinsa.admin.service.exceptions.NotPermittedException;
+import com.flab.yousinsa.user.controller.annotation.AuthSession;
+import com.flab.yousinsa.user.controller.annotation.SignInUser;
+import com.flab.yousinsa.user.domain.dtos.AuthUser;
+import com.flab.yousinsa.user.domain.enums.UserRole;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class AdminRequestStoreController {
+
+	private final AdminStoreRequestService storeRequestService;
+
+	public AdminRequestStoreController(AdminStoreRequestService storeRequestService) {
+		this.storeRequestService = storeRequestService;
+	}
+
+	@AuthSession
+	@PutMapping("api/admin/v1/stores/{storeId}")
+	public ResponseEntity<RequestStoreDtoResponse> acceptStoreRequest(
+		@PathVariable("storeId") Long storeId,
+		@SignInUser AuthUser user
+	) {
+		isAuthUserAdmin(user);
+
+		RequestStoreDtoResponse response = storeRequestService.acceptStoreRequest(storeId);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+	}
+
+	private void isAuthUserAdmin(AuthUser user) {
+		if (user.getUserRole() != UserRole.ADMIN) {
+			throw new NotPermittedException("store request can be accepted by admin");
+		}
+	}
+}
